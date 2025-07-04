e53ffe5ba784918eba42a027c314bec1369d308e
Nico Vece <me@nicovece.com>
2025-07-04 19:14:50 +0200

    Implements recipe creation and storage
    Creates a script that allows users to input recipe details,
    including name, ingredients, cooking time, and difficulty.
    Stores these recipes in a binary file using pickle.
    
    Retrieves and displays the saved recipes.

--- exercise-1_4/1_4-practice_task_2/recipe_creator.py
+++ exercise-1_4/1_4-practice_task_2/recipe_creator.py
@@ -1,21 +1,34 @@
 import pickle
 
-recipe = {
-    'name': 'Tea',
-    'ingredients': ['tea', 'water', 'sugar'],
-    'cooking_time': '5 minutes',
-    'difficulty': 'easy'
-}
+recipes = []
 
-recipe_file = open('exercise-1_4/1_4-practice_task_2/recipe_binary.bin', 'wb')
-pickle.dump(recipe, recipe_file)
-recipe_file.close()
+while True:
+    name = input("Enter the recipe name: ")
+    ingredients = input("Enter the ingredients (comma separated): ").split(',')
+    cooking_time = input("Enter the cooking time: ")
+    difficulty = input("Enter the difficulty: ")
 
-with open('exercise-1_4/1_4-practice_task_2/recipe_binary.bin', 'rb') as my_file:
-    recipe = pickle.load(my_file)
+    recipe = {
+        'name': name,
+        'ingredients': [ingredient.strip() for ingredient in ingredients],
+        'cooking_time': cooking_time,
+        'difficulty': difficulty
+    }
+    recipes.append(recipe)
+    
+    another = input("Add another recipe? (y/n): ")
+    if another.lower() != 'y':
+        break
 
-print("Recipe details - ")
-print("Name:  " + recipe['name'])
-print("Ingredients: " + str(recipe['ingredients']))
-print("Cooking time: " + recipe['cooking_time'])
-print("Difficulty: " + recipe['difficulty'])
+with open('exercise-1_4/1_4-practice_task_2/recipes.bin', 'wb') as file:
+    pickle.dump(recipes, file)
+
+with open('exercise-1_4/1_4-practice_task_2/recipes.bin', 'rb') as file:
+    loaded_recipes = pickle.load(file)
+
+for recipe in loaded_recipes:
+    print("Recipe details - ")
+    print("Name:  " + recipe['name'])
+    print("Ingredients: " + str(recipe['ingredients']))
+    print("Cooking time: " + recipe['cooking_time'])
+    print("Difficulty: " + recipe['difficulty'])
